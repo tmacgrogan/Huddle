edf779e5bc687687b19e4c4510caee6f48fe2a0a
jcole123 <jcole12321@gmail.com>
2015-10-19 19:03:47 -0400

    Changed attribute names so they actually make sense

--- www/js/services.js
+++ www/js/services.js
@@ -6,56 +6,49 @@ angular.module('starter.services', [])
   // Some fake testing data
   var chats = [{
     id: 0,
-    name: 'Tennis',
-    lastText: 'CRC',
+    title: 'Tennis',
+    location: 'CRC',
     description: 'Looking for someone to play tennis with me later today. We can hit balls and talk about the Williams sisters, or whatever people who play Tennis do.',
     time: 'Today, 4:00pm',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
   }, {
     id: 1,
-    name: 'Pickup Soccer',
-    lastText: 'CRC',
+    title: 'Pickup Soccer',
+    location: 'CRC',
     description: 'blah',
     time: 'Today, 5:00pm',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
   }, {
     id: 2,
-    name: 'Calc II HW5 Help',
-    lastText: 'CULC',
+    title: 'Calc II HW5 Help',
+    location: 'CULC',
     description: 'blah',
     time: 'Tomorrow, 1:00pm',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
   }, {
     id: 3,
-    name: 'CS 2110 Recursive Assembly Help',
-    lastText: 'College of Computing',
+    title: 'CS 2110 Recursive Assembly Help',
+    location: 'College of Computing',
     description: 'blah',
     time: 'Tomorrow, 9:00pm',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
   }, {
     id: 4,
-    name: 'Need Members for Hackathon Group',
-    lastText: 'Klaus Atrium',
+    title: 'Need Members for Hackathon Group',
+    location: 'Klaus Atrium',
     description: 'blah',
     time: 'Tomorrow, 6:00pm',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
   },
-      {
-          id: 5,
-          name: 'Teach me how to swim',
-          lastText: 'Florida',
-          description: 'Sharks',
-          time: 'Today, 6:00am',
-          face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-      },
-      {
-          id: 6,
-          name: 'Free juggling lessons',
-          lastText: 'Skiles',
-          description: 'blah',
-          time: 'Tomorrow, 4:00pm',
-          face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-      }];
+  {
+    id: 5,
+    title: 'Teach me how to swim',
+    location: 'Florida',
+    description: 'Sharks',
+    time: 'Today, 6:00am',
+  },
+  {
+    id: 6,
+    title: 'Free juggling lessons',
+    location: 'Skiles',
+    description: 'blah',
+    time: 'Tomorrow, 4:00pm',
+  }];
 
   return {
     all: function() {
